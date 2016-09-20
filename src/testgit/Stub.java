f6fee9b74593f684b8ab958e1c35b0cb28557119
kochetov <kochetov@itopcase.ru>
2016-09-20 20:42:04 +0300

    1

--- src/testgit/Stub.java
+++ src/testgit/Stub.java
@@ -4,7 +4,7 @@ public class Stub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub2
-		System.out.println("gitTest1245");
+		System.out.println("gitTest1");
 	}
 
 }
